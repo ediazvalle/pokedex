ff5b40bb008c426146fc75b940fa5d4c63817803
Eduardo Diaz Valle <70858850+ediazvalle@users.noreply.github.com>
2023-08-01 21:48:41 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import PokemonList from './PokemonList';
+import Pagination from "./pagination";
+import axios from 'axios';
+
+function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPage, setCurrentPage] = useState(["https://pokeapi.co/api/v2/pokemon"])
+  const [nextPage, setNextPage] = useState()
+  const [prevPage, setPrevPage] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+      setLoading(true)
+      let cancel
+      axios.get(currentPage, {
+        cancelToken: new axios.CancelToken(c => cancel = c)
+      }).then(res => {
+        setLoading(false)
+        setNextPage(res.data.next)
+        setPrevPage(res.data.previous)
+        setPokemon(res.data.results.map(pokemon => pokemon.name))
+      })
+
+      return () => cancel()
+  }, [currentPage])
+
+  function gotoNextPage() {
+    setCurrentPage(nextPage)
+  }
+
+  function gotoPrevPage() {
+    setCurrentPage(prevPage)
+  }
+
+  if (loading) return "Loading..."
+
+  return (
+    <>
+      <PokemonList pokemon={pokemon}/>
+      <Pagination
+        gotoNextPage={nextPage ? gotoNextPage : null}
+        gotoPrevPage={prevPage ? gotoPrevPage : null}
+      />
+    </>
+  )
+}
+
+export default App
